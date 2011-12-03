f396b4796fba75f00288caf3a4a2bb5efac74c7f
Daniel Joseph <getoveritdan@gmail.com>
2011-12-02 23:00:37 -0500

    allow deletes

--- app/models/guest.rb
+++ app/models/guest.rb
@@ -1,5 +1,5 @@
 class Guest < ActiveRecord::Base
   validates_uniqueness_of :ticketnumber
   validates_presence_of :name, :ticketnumber
-  validates_format_of :ticketnumber, :with => /^\d{1,3}/i, :message => "must be a three digit number."
+  validates_format_of :ticketnumber, :with => /^\d{1,3}$/i, :message => "must be a three digit number."
 end
